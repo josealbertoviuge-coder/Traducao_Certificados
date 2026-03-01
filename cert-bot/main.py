d5e87a9370bb4af98bbd4d91859f61f66f2674aa
josealbertoviuge-coder <josealbertoviuge@gmail.com>
2026-03-01 14:03:21 -0300

    Update main.py

--- cert-bot/main.py
+++ cert-bot/main.py
@@ -1,1 +1,9 @@
+from drive_service import conectar_drive
+from processor import processar
 
+def main():
+    drive = conectar_drive()
+    processar(drive)
+
+if __name__ == "__main__":
+    main()
