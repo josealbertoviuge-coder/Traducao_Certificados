17ff8cf99f40bf3e933ba6120ff5f8c07fab520e
josealbertoviuge-coder <josealbertoviuge@gmail.com>
2026-03-01 14:02:33 -0300

    Update processor.py

--- cert-bot/processor.py
+++ cert-bot/processor.py
@@ -1,1 +1,41 @@
+from config import ID_ENTRADA, ID_TRADUZIDOS, ID_PROCESSADOS
+from pdf_utils import extrair_texto
+from translator import traduzir
+from pdf_utils import gerar_pdf
 
+def listar_arquivos(drive):
+    return drive.ListFile(
+        {'q': f"'{ID_ENTRADA}' in parents and trashed=false"}
+    ).GetList()
+
+def baixar_arquivo(file):
+    file.GetContentFile(file['title'])
+    return file['title']
+
+def enviar_traduzido(drive, caminho):
+    f = drive.CreateFile({'parents': [{'id': ID_TRADUZIDOS}]})
+    f.SetContentFile(caminho)
+    f.Upload()
+
+def mover_para_processados(file):
+    file['parents'] = [{'id': ID_PROCESSADOS}]
+    file.Upload()
+
+def processar(drive):
+    arquivos = listar_arquivos(drive)
+
+    for arquivo in arquivos:
+        print("Processando:", arquivo['title'])
+
+        caminho = baixar_arquivo(arquivo)
+
+        texto = extrair_texto(caminho)
+
+        texto_traduzido = traduzir(texto)
+
+        nome_saida = "EN_" + arquivo['title']
+        gerar_pdf(texto_traduzido, nome_saida)
+
+        enviar_traduzido(drive, nome_saida)
+
+        mover_para_processados(arquivo)
