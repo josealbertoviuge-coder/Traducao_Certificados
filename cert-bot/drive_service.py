d7517392f63f98338638e24438dbf4e933af40b9
josealbertoviuge-coder <josealbertoviuge@gmail.com>
2026-03-01 13:55:57 -0300

    Update drive_service.py

--- cert-bot/drive_service.py
+++ cert-bot/drive_service.py
@@ -1,1 +1,7 @@
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
 
+def conectar_drive():
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()  # abre login na primeira vez
+    return GoogleDrive(gauth)
