9fedc5a2d30872476dbf0bb9f4c15646b73eb834
josealbertoviuge-coder <josealbertoviuge@gmail.com>
2026-03-01 23:10:49 -0300

    Update drive_service.py

--- cert-bot/drive_service.py
+++ cert-bot/drive_service.py
@@ -1,17 +1,30 @@
 import os
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
 import json
-from pydrive2.auth import GoogleAuth
-from pydrive2.drive import GoogleDrive
-from oauth2client.service_account import ServiceAccountCredentials
+
+SCOPES = ['https://www.googleapis.com/auth/drive']
 
 def conectar_drive():
-    gauth = GoogleAuth()
 
-    scope = ['https://www.googleapis.com/auth/drive']
-    creds_json = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"])
+    creds = None
 
-    gauth.credentials = ServiceAccountCredentials.from_json_keyfile_dict(
-        creds_json, scope
-    )
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
 
-    return GoogleDrive(gauth)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_config(
+                json.loads(os.environ["GOOGLE_CREDENTIALS"]),
+                SCOPES
+            )
+            creds = flow.run_console()  # 🔥 funciona sem navegador
+
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    return build('drive', 'v3', credentials=creds)
