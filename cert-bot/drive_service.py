d41648c62612ebfef2f024b1f91a34d1f8502d6c
josealbertoviuge-coder <josealbertoviuge@gmail.com>
2026-03-01 21:18:50 -0300

    Update drive_service.py

--- cert-bot/drive_service.py
+++ cert-bot/drive_service.py
@@ -1,7 +1,13 @@
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
+from oauth2client.service_account import ServiceAccountCredentials
 
 def conectar_drive():
     gauth = GoogleAuth()
-    gauth.LocalWebserverAuth()
+
+    scope = ['https://www.googleapis.com/auth/drive']
+    gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        'service_account.json', scope
+    )
+
     return GoogleDrive(gauth)
