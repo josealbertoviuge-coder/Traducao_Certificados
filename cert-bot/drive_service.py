9fdc2a9473287b5ed43199a695fc0c8c1d79fe8b
josealbertoviuge-coder <josealbertoviuge@gmail.com>
2026-03-01 23:37:38 -0300

    Update drive_service.py

--- cert-bot/drive_service.py
+++ cert-bot/drive_service.py
@@ -1,27 +1,23 @@
 import os
 import json
 from google_auth_oauthlib.flow import Flow
-from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
 def conectar_drive():
 
-    if "GOOGLE_AUTH_CODE" in os.environ:
+    if "GOOGLE_AUTH_CODE" not in os.environ:
+        raise Exception("Variável GOOGLE_AUTH_CODE não definida")
 
-        flow = Flow.from_client_config(
-            json.loads(os.environ["GOOGLE_CREDENTIALS"]),
-            scopes=SCOPES,
-            redirect_uri="http://localhost"
-        )
+    flow = Flow.from_client_config(
+        json.loads(os.environ["GOOGLE_CREDENTIALS"]),
+        scopes=SCOPES,
+        redirect_uri="http://localhost"
+    )
 
-        flow.fetch_token(code=os.environ["GOOGLE_AUTH_CODE"])
-        creds = flow.credentials
+    flow.fetch_token(code=os.environ["GOOGLE_AUTH_CODE"])
 
-        return build('drive', 'v3', credentials=creds)
-
-    raise Exception("GOOGLE_AUTH_CODE não definido")
-            token.write(creds.to_json())
+    creds = flow.credentials
 
     return build('drive', 'v3', credentials=creds)
